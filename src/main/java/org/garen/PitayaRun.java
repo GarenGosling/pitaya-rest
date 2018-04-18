5630380beea98f1ed059ba25a55d6a0acac45b3b
liuxueliang <liuxueliang@yingu.com>
2018-04-18 18:15:04 +0800

    初始化程序，拉下了一些文件

---
+++ src/main/java/org/garen/PitayaRun.java
@@ -0,0 +1,75 @@
+package org.garen;
+
+
+import org.garen.pitaya.filter.LoginFilter;
+import org.garen.pitaya.swagger.api.ApiOriginFilter;
+import org.garen.pitaya.swagger.api.ApiOriginFilter;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.Filter;
+
+
+/**
+ * Created by Administrator on 2017/8/23.
+ */
+@Controller
+@SpringBootApplication
+@MapperScan("org.garen.pitaya.mybatis.mapper")
+public class PitayaRun {
+
+    /**
+     * 配置过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean someFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(apiOriginFilter());
+        registration.addUrlPatterns("/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("apiOriginFilter");
+        return registration;
+    }
+
+    /**
+     * 配置过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean loginFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(loginFilter());
+        registration.addUrlPatterns("/api/*");
+        registration.addInitParameter("paramName", "paramValue");
+        registration.setName("loginFilter");
+        registration.setOrder(Integer.MAX_VALUE);
+        return registration;
+    }
+
+    /**
+     * 创建一个bean
+     * @return
+     */
+    @Bean(name = "apiOriginFilter")
+    public Filter apiOriginFilter() {
+        return new ApiOriginFilter();
+    }
+    /**
+     * 创建一个bean
+     * @return
+     */
+    @Bean(name = "loginFilter")
+    public Filter loginFilter() {
+        return new LoginFilter();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PitayaRun.class, args);
+    }
+
+}
---
+++ src/main/java/org/garen/pitaya/filter/LoginFilter.java
@@ -0,0 +1,36 @@
+package org.garen.pitaya.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.garen.pitaya.swagger.model.BaseModel;
+import org.garen.pitaya.swagger.model.ResponseModel;
+import org.garen.pitaya.util.JsonMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2017-08-24T01:49:38.472Z")
+
+public class LoginFilter extends BaseModel implements Filter {
+
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+}
