ff27308217e91d87a907e8e70d29e20b338c8698
liuxueliang <liuxueliang@yingu.com>
2018-05-16 17:54:32 +0800

    角色管理业务代码

--- src/main/java/org/garen/pitaya/service/SysRoleManage.java
+++ src/main/java/org/garen/pitaya/service/SysRoleManage.java
@@ -1,10 +1,20 @@
 package org.garen.pitaya.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.garen.pitaya.mybatis.domain.SysRole;
 import org.garen.pitaya.mybatis.domain.SysRoleQuery;
+import org.garen.pitaya.mybatis.domain.SysUser;
 import org.garen.pitaya.mybatis.service.SysRoleService;
+import org.garen.pitaya.swagger.model.SysRoleVo;
+import org.garen.pitaya.util.EsapiUtil;
+import org.garen.pitaya.util.TransferUtil;
+import org.garen.pitaya.valid.SysRoleValid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SysRoleManage extends BaseManage<Long>{
@@ -14,6 +24,125 @@ public class SysRoleManage extends BaseManage<Long>{
     @Override
     public SysRoleService<SysRole, SysRoleQuery, Long> getService() {
         return service;
+    }
+
+    @Autowired
+    SysRoleValid sysRoleValid;
+
+    /**
+     * 新增
+     * @param sysRoleVo
+     * @return
+     */
+    public boolean save(SysRoleVo sysRoleVo){
+        // 校验入参
+        sysRoleValid.saveValid(sysRoleVo);
+        // 对象转化
+        SysRole sysRole = new SysRole();
+        TransferUtil.transfer(sysRole, sysRoleVo);
+        // 持久化
+        int i = create(sysRole);
+        // 返回结果
+        if(i == 1){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 修改
+     * @param sysRoleVo
+     * @return
+     */
+    public boolean update(SysRoleVo sysRoleVo){
+        // 校验入参
+        sysRoleValid.updateValid(sysRoleVo);
+        // 对象转化
+        SysRole sysRole = new SysRole();
+        TransferUtil.transfer(sysRole, sysRoleVo);
+        // 持久化
+        int i = modify(sysRole);
+        // 返回结果
+        if(i == 1){
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 批量删除
+     * @param ids
+     * @return
+     */
+    public String batchRemove(String ids){
+        int count = 0;
+        List<String> failList = new ArrayList<>();
+        for(String id : ids.split(",")){
+            int i = removeById(Long.parseLong(id));
+            if(i == 1){
+                count ++;
+            }else{
+                SysRole byID = getService().findByID(Long.parseLong(id));
+                failList.add(byID.getName());
+            }
+        }
+        return failMsg(count, ids, failList);
+    }
+    
+
+    public List<SysRole> getByProject(String project) {
+        return getListByParams(null, null, project);
+    }
+
+    public List<SysRole> getByName(String name){
+        return getListByParams(null, name, null);
+    }
+
+    public SysRole getByNameEqual(String name){
+        SysRoleQuery query = new SysRoleQuery();
+        SysRoleQuery.Criteria criteria = query.createCriteria();
+        criteria.andNameEqualTo(name);
+        List<SysRole> by = getService().findBy(query);
+        if(!CollectionUtils.isEmpty(by)){
+            return by.get(0);
+        }
+        return null;
+    }
+
+    /**
+     * 通过编码查询
+     * @param code
+     * @return
+     */
+    public SysRole getByCode(String code){
+        List<SysRole> listByParams = getListByParams(code, null, null);
+        if(!CollectionUtils.isEmpty(listByParams)){
+            return null;
+        }
+        return listByParams.get(0);
+    }
+
+    /**
+     * 通过属性查询多个用户，组合查询方式“与”
+     * @param code
+     * @param name
+     * @param project
+     * @return
+     */
+    private List<SysRole> getListByParams(String code, String name, String project){
+        SysRoleQuery query = new SysRoleQuery();
+        SysRoleQuery.Criteria criteria = query.createCriteria();
+        if(StringUtils.isNotBlank(code)){
+            criteria.andCodeEqualTo(EsapiUtil.sql(code));
+        }
+        if(StringUtils.isNotBlank(name)){
+            criteria.andNameLike("%"+ EsapiUtil.sql(name)+"%");
+        }
+        if(StringUtils.isNotBlank(project)){
+            criteria.andProjectEqualTo(project);
+        }
+        return getService().findBy(query);
     }
 
 
---
+++ src/main/java/org/garen/pitaya/swagger/model/SysRoleVo.java
@@ -0,0 +1,12 @@
+package org.garen.pitaya.swagger.model;
+
+import lombok.Data;
+
+@Data
+public class SysRoleVo {
+    private Long id;
+    private String code;
+    private String name;
+    private String resourceIds;
+    private String project;
+}
